f3f9aa68c33268059710ecbecf135e08ded98b61
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2021-12-09 13:23:36 +0700

    Change value blue color

--- xenshop_core/lib/base/colors.dart
+++ xenshop_core/lib/base/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const blue = Color(0xff4373FC);
+const blue = Color(0xff436BB4);
 const red = Color(0xffD4525D);
 const black = Color(0xff000000);
 const white = Color(0xffFFFFFF);
